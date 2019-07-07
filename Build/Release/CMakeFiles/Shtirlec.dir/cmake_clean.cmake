8829e0d3932fdddf5d3996f5116a8789c53c7422
root <root@SensorsDataServer.TERMALSENSORS>
2019-07-07 16:32:33 +0300

    Реализованно разделение буфера с данными на id и val, но не оттесченно

---
+++ Build/Release/CMakeFiles/Shtirlec.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shtirlec.dir/source/Пример/Shtirlec.cpp.o"
+  "Shtirlec.pdb"
+  "Shtirlec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Shtirlec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
