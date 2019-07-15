c214b45509154f5e12985c9e96079596ae2d2950
localuser <localuser@SensorsDataServer.TERMALSENSORS>
2019-07-15 16:43:17 +0300

    Первая рабочая версия программы для комуникации приёмной станции с сервером, с последующей передачей данных в web-интерфейс

---
+++ build/Release/CMakeFiles/Shtirlec.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/localuser/Документы/Projects/Shtirlec/source/init_serial_port.c" "/home/localuser/Документы/Projects/Shtirlec/build/Release/CMakeFiles/Shtirlec.dir/source/init_serial_port.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/localuser/Документы/Projects/Shtirlec/source/Main.cpp" "/home/localuser/Документы/Projects/Shtirlec/build/Release/CMakeFiles/Shtirlec.dir/source/Main.cpp.o"
+  "/home/localuser/Документы/Projects/Shtirlec/source/SerialPort.cpp" "/home/localuser/Документы/Projects/Shtirlec/build/Release/CMakeFiles/Shtirlec.dir/source/SerialPort.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Release/CMakeFiles/Shtirlec.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Shtirlec.dir/source/Main.cpp.o"
+  "CMakeFiles/Shtirlec.dir/source/SerialPort.cpp.o"
+  "CMakeFiles/Shtirlec.dir/source/init_serial_port.c.o"
+  "Shtirlec.pdb"
+  "Shtirlec"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Shtirlec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
